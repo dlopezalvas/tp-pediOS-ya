09aef0b1c524b96b54de1bf38568ca0fa9c56278
candelasierra <candela.sierra98@gmail.com>
2020-09-04 00:47:11 -0300

    deserializar_mensaje

--- commonsCoronaLinux/serializacion.h
+++ commonsCoronaLinux/serializacion.h
@@ -115,6 +115,7 @@ typedef struct{
 
 void enviar_mensaje(t_mensaje* mensaje, int socket);
 void* serializar_paquete(t_paquete* paquete, int *bytes);
+void* deserializar_mensaje(void* buffer, op_code tipo_mensaje);
 t_buffer* cargar_buffer(t_mensaje* mensaje);
 int tamanio_lista_strings(t_list* lista_de_strings);
 int tamanio_lista_cocineroAfinidad(t_list* lista_de_strings);
@@ -148,22 +149,22 @@ t_coordenadas* deserializar_posicion(void* buffer);
 
 
 t_log* iniciar_logger(t_config*);
-char* t_mensaje_to_string(t_mensaje* mensaje);
-void loggear_mensaje_enviado(t_mensaje* mensaje, t_log* logger);
-void loggear_mensaje_recibido(t_mensaje* mensaje, t_log* logger);
-char* vacio_to_string(t_mensaje* mensaje);
-char* restaurante_y_plato_to_string(t_mensaje* mensaje);
-char* seleccionar_restaurante_to_string(t_mensaje* mensaje);
-char* id_o_confirmacion_to_string(t_mensaje* mensaje);
+char* t_mensaje_to_string(void* mensaje, op_code tipo_mensaje);
+void loggear_mensaje_enviado(void* mensaje, op_code tipo_mensaje, t_log* logger);
+void loggear_mensaje_recibido(void* mensaje, op_code tipo_mensaje, t_log* logger);
+char* vacio_to_string(op_code tipo_mensaje);
+char* restaurante_y_plato_to_string(t_restaurante_y_plato* restaurante_plato, op_code tipo_mensaje);
+char* seleccionar_restaurante_to_string(m_seleccionarRestaurante * seleccionar_restaurante, op_code tipo_mensaje);
+char* id_o_confirmacion_to_string(uint32_t * id_confirmacion, op_code tipo_mensaje);
 bool es_id(op_code tipo_mensaje);
 char* bool_to_string(bool confirmacion);
-char* nombre_to_string(t_mensaje* mensaje);
-char* rta_obtener_restaurante_to_string(t_mensaje* mensaje);
-char* nombre_y_id_to_string(t_mensaje* mensaje);
-char* plato_listo_to_string(t_mensaje* mensaje);
-char* rta_consultar_pedido_to_string(t_mensaje* mensaje);
-char* rta_obtener_pedido_to_string(t_mensaje* mensaje);
-char* guardar_plato_to_string(t_mensaje* mensaje);
+char* nombre_to_string(t_nombre* nombre, op_code tipo_mensaje);
+char* rta_obtener_restaurante_to_string(rta_obtenerRestaurante* obtener_restaurante, op_code tipo_mensaje);
+char* nombre_y_id_to_string(t_nombre_y_id * nombre_id, op_code tipo_mensaje);
+char* plato_listo_to_string(m_platoListo * plato_listo, op_code tipo_mensaje);
+char* rta_consultar_pedido_to_string(rta_consultarPedido* consultar_pedido, op_code tipo_mensaje);
+char* rta_obtener_pedido_to_string(rta_obtenerPedido* obtener_pedido, op_code tipo_mensaje);
+char* guardar_plato_to_string(m_guardarPlato* guardar_plato, op_code tipo_mensaje);
 
 
 #endif /* serializacion_h */
